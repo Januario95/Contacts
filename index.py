4dd6f76e515f6048a7786daf1c448c28a6e17872
Januario95 <januariocipriano@gmail.com>
2021-07-29 05:14:07 +0800

    Create a Person class

--- index.py
+++ index.py
@@ -9,3 +9,10 @@ Created at Thur Jul 29 2021 05:07:23
 import time
 
 print(time.ctime())
+
+
+class Person(object):
+    def __init__(self, name, age, social_sec_nr):
+        self.__name = name
+        self.__age = age
+        self.__social_sec_nr = social_sec_nr
