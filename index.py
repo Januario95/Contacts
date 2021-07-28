8be17581ccf905e5541f13094aa17e3f3836e8c1
Januario95 <januariocipriano@gmail.com>
2021-07-29 05:28:44 +0800

    Add age method

--- index.py
+++ index.py
@@ -25,6 +25,14 @@ class Person(object):
     def name(self, new_name):
         self.__name = new_name
 
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, new_age):
+        self.__age = new_age
+
 
 person = Person('Olga Matias', 24, 235234)
-print(person.name)
+print(person.age)
